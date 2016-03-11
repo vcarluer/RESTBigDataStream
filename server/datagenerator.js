6ec5dbfc4a1ea475ed46386e04fcff7b5dd3531a
Vincent Carluer <vcarluer@gmail.com>
2016-03-11 16:00:23 +0100

    data test

--- server/datagenerator.js
+++ server/datagenerator.js
@@ -1,5 +1,33 @@
 module.exports = {
 	getSimpleData: function() {
 		return { data: 'test' }
-	}	
+	},
+
+	getJSON: function(size, count) {
+		var json
+		if (count) {
+			var obj = {
+				data: []
+			}
+
+			for (var i = 0; i < count; i++) {
+				obj.data[i] = ''
+				for (var j = 0; j < size; j++) {
+					obj.data[i] += 'a'
+				}
+			}
+
+			json = JSON.stringify(obj)
+		} else {
+			json = '{'
+			for(var i = 0; i < size - 2; i++) {
+				json += 'a'
+			}
+
+			json += '}'
+		}
+	
+
+		return json
+	}
 }
--- server/test/bigdatatest.js
+++ server/test/bigdatatest.js
@@ -8,4 +8,21 @@ describe('Big data set creation test', () => {
 		expect(data).to.exist
 		expect(data.data).to.equal('test')
 	})
+
+	it('should return a json string of length 10', () => {
+		var json = dataGenerator.getJSON(10)
+		expect(json.length).to.equal(10)
+	})
+
+	it ('should return a json string of length 1000', () => {
+		var json = dataGenerator.getJSON(1000)
+		expect(json.length).to.equal(1000)
+	})
+
+	it ('should return a json an array of 1000 items of length 10', () => {
+		var json = dataGenerator.getJSON(10, 1000)
+		var obj = JSON.parse(json)
+		expect(obj.data.length).to.equal(1000)
+		expect(obj.data[0].length).to.equal(10)
+	})
 })
