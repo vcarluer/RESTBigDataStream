3254be907e9da5f15002f7b98680b7cd3928e1d5
Vincent Carluer <vcarluer@gmail.com>
2016-03-11 17:52:50 +0100

    first working client

--- client/RESTBigDataClient/RESTBigDataClient/Program.cs
+++ client/RESTBigDataClient/RESTBigDataClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DynamicRestProxy.PortableHttpClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,22 @@ using System.Threading.Tasks;
 
 namespace RESTBigDataClient
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
+			var t = TestData();
+			t.Wait();
+		}
+		private static async Task TestData()
+		{
+			using (dynamic localrest = new DynamicRestClient("http://localhost:8072/"))
+			{
+				dynamic testData = await localrest.get();
+				Console.WriteLine("Count: " + testData.data.Count);
+				Console.WriteLine("Count: " + testData.data[0].prop0);
+
+			}
 		}
 	}
 }
