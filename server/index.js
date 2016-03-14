51f96d08b90636e82a2e4704da26216b00516b5c
Vincent Carluer <vcarluer@gmail.com>
2016-03-14 12:13:19 +0100

    stream data server + vscode launchers

--- server/index.js
+++ server/index.js
@@ -1,22 +1,37 @@
 var http = require('http')
 var datagenerator = require('./datagenerator')
 var url = require('url')
+var fs = require('fs')
+var path = require('path')
+var util = require('util')
 
 var server = http.createServer((request, response) => {
 	var reqUrl = url.parse(request.url)
-	var size = 10
-	if (reqUrl.pathname && reqUrl.pathname !== '/') {
-		size = reqUrl.pathname.substr(1)
-	}
+	if (reqUrl.pathname.substr(0, 5) === '/data') {
+		var respath = path.join('.', reqUrl.pathname)
+		console.log(util.format('streaming %s', respath))
+		var stream = fs.createReadStream(respath)
+		stream.pipe(response)
 
-	if (isNaN(size)) {
-		response.writeHead('403', 'Size not a number')
-		response.end()
+		stream.on('end', () => {
+			response.end()
+			console.log('stream end')
+		})
 	} else {
-		response.writeHead(200, {'Content-Type': 'application/json'})
-		var json = datagenerator.getMillions(size)
-		response.write(json)
-		response.end()
+		var size = 10
+		if (reqUrl.pathname && reqUrl.pathname !== '/') {
+			size = reqUrl.pathname.substr(1)
+		}
+
+		if (isNaN(size)) {
+			response.writeHead('403', 'Size not a number')
+			response.end()
+		} else {
+			response.writeHead(200, {'Content-Type': 'application/json'})
+			var json = datagenerator.getMillions(size)
+			response.write(json)
+			response.end()
+		}
 	}
 })
 
