c019f610ece7739a5089dbfbf44317a1229f948d
Vincent Carluer <vcarluer@gmail.com>
2016-03-15 11:22:59 +0100

    JSON stream reader

--- client/RESTBigDataClient/RESTBigDataClient/Program.cs
+++ client/RESTBigDataClient/RESTBigDataClient/Program.cs
@@ -1,8 +1,11 @@
 ﻿using DynamicRestProxy.PortableHttpClient;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,23 +15,64 @@ namespace RESTBigDataClient
 	{
 		static void Main(string[] args)
 		{
-			var t = TestData();
-			t.Wait();
+			TestData();
 		}
-		private static async Task TestData()
-		{
+		private static void TestData()
+		{			
 			Stopwatch watch = new Stopwatch();
 			watch.Start();
-			using (dynamic localrest = new DynamicRestClient("http://localhost:8072/500"))
-			{				
-				dynamic testData = await localrest.get();
-				watch.Stop();
-				Console.WriteLine(String.Format("Data retrieved in {0} ms", watch.ElapsedMilliseconds));
-				Console.WriteLine("Count: " + testData.data.Count);
-				Console.WriteLine("Prop0 of first object: " + testData.data[0].prop0);
-				Console.WriteLine("Prop9 of last object: " + testData.data[testData.data.Count -1].prop9);
-				Console.ReadKey();
+
+			Task t = null;
+			if (mode == 0) t = TestWithDynamicRestClient();
+			if (mode == 1) t = TestWithHttpClientAndStream();
+
+			if (t != null)
+			{
+				t.Wait();
 			}
+			
+			watch.Stop();
+			Console.WriteLine(String.Format("Data retrieved in {0} ms", watch.ElapsedMilliseconds));
+
+			Console.ReadKey();
+
+		}
+
+		private static int mode = 1;
+		private static string url = "http://localhost:8072/300";
+
+		private static async Task TestWithDynamicRestClient()
+		{
+			using (dynamic localrest = new DynamicRestClient(url))
+			{
+				dynamic testData = await localrest.get();
+				WriteData(testData);
+			}
+		}
+
+		private static async Task TestWithHttpClientAndStream()
+		{
+			// http://www.newtonsoft.com/json/help/html/Performance.htm
+			using (HttpClient httpClient = new HttpClient())
+			{
+				dynamic testData;
+				using (Stream stream = httpClient.GetStreamAsync(url).Result)
+				using (StreamReader streamReader = new StreamReader(stream))
+				using (JsonReader jsonReader = new JsonTextReader(streamReader))
+				{
+					JsonSerializer serializer = new JsonSerializer();
+					testData = serializer.Deserialize(jsonReader);
+				}
+
+				WriteData(testData);
+			}
+		}
+
+		private static void WriteData(dynamic testData)
+		{
+			Console.WriteLine("Count: " + testData.data.Count);
+			Console.WriteLine("Prop0 of first object: " + testData.data[0].prop0);
+			Console.WriteLine("Prop9 of last object: " + testData.data[testData.data.Count - 1].prop9);
 		}
 	}
 }
